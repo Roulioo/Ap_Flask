58992fef965c0afddca8ca7714c3cc9b545d47e7
YOUR NAME <YOUR EMAIL ADRESS>
2019-02-21 11:39:58 +0100

    Add documents

--- run.py
+++ run.py
@@ -1,39 +1,43 @@
 # on importe flask
 
-from flask import Flask, render_template 
+from flask import Flask, render_template
 import utils as u
 
-# render permet de gerer la partit template 
+# render permet de gerer la partit template
 
-app = Flask(__name__) # objet 
+app = Flask(__name__)  # objet
+
+# on creer une simple variable avec notre donnee
+
+users = [
+    {'name': 'Hero', 'grade': 1, 'notes': [15, 17, 11, 9, 10]},
+    {'name': 'Dunn', 'grade': 2, 'notes': [10, 11, 11, 19, 7]},
+    {'name': 'Sue', 'grade': 2, 'notes': [13, 17, 9, 9, 7]},
+    {'name': 'Thor', 'grade': 1, 'notes': [15, 17, 20, 19, 16]},
+    {'name': 'Hicks', 'grade': 1, 'notes': [13, 10, 11, 6, 12]},
+]
+
 
 # premiere route
-@app.route('/') # decorateur 
-
+@app.route('/')  # decorateur
 def index():
-
-    # on creer une simple variable avec notre donnee
-
-    users =  [
-        {'name' : 'Hero', 'grade' : 1, 'notes' : [15,17,11,9,10] },
-        {'name' : 'Dunn', 'grade' : 2, 'notes' : [10,11,11,19,7] },
-        {'name' : 'Sue', 'grade' : 2, 'notes' : [13,17,9,9,7] },
-        {'name' : 'Thor', 'grade' : 1, 'notes' : [15,17,20,19,16] },
-        {'name' : 'Hicks', 'grade' : 1, 'notes' : [13,10,11,6,12] },
-    ]
 
     # dans data, on stocke notre variable et title de meme
 
-    return render_template('index.html', data = users, title="Liste des etudiants", average = u.moyenne) 
+    return render_template('index.html', data=users, title="Liste des etudiants", average=u.moyenne)
 
 # deuxieme route
 
-@app.route('/contact') # dans la console avec /contact on accede a cette page 
 
+@app.route('/contact')  # dans la console avec /contact on accede a cette page
 def contact():
-    return 'Page contact' # on affiche "page contact"
+    return 'Page contact'  # on affiche "page contact"
+
+
+@app.route('/best')  # dans la console avec /contact on accede a cette page
+def best():
+    return render_template('best.html', maximum=u.moyenne_max, data = users)
 
 
 if __name__ == "__main__":
     app.run()
-    
--- utils.py
+++ utils.py
@@ -7,4 +7,17 @@ def moyenne(l):
     if len(l) == 0:
         raise "Division by zero"
 
-    return sum(l)/len(l) 
+    return round(sum(l)/len(l))
+
+def moyenne_max(users):
+    
+    maximum, name = 0, ''
+
+    for user in users:
+
+        m = moyenne(user['notes'])
+
+        if m > maximum:
+            maximum, name = m, user['name']
+    
+    return maximum, name
